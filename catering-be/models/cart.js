651eb96b8c25adb0d6e6cef9a3c9f1226f58e5e7
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-03-21 04:35:31 +0700

    update: models cart

--- catering-be/models/cart.js
+++ catering-be/models/cart.js
@@ -14,6 +14,11 @@ module.exports = (sequelize, DataTypes) => {
   }
   Cart.init(
     {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
       user_id: DataTypes.UUID,
     },
     {
