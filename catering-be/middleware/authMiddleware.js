6a74be19bffff25685185409ec5e7f6488ca37a2
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-03-21 22:23:17 +0700

    update: modify authMiddleware for admin

--- catering-be/middleware/authMiddleware.js
+++ catering-be/middleware/authMiddleware.js
@@ -23,7 +23,7 @@ exports.authenticate = (req, res, next) => {
   });
 };
 
-exports.authorize = (roles) => {
+exports.authorize = (roles = []) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
       return res.status(403).json({ message: "You are not authorized!" });
