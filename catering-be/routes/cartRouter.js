49ce713b6ca4b59f8bc147b16166b5e4388a8119
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-04-01 23:34:46 +0700

    update: cartRouter to handle cart

--- catering-be/routes/cartRouter.js
+++ catering-be/routes/cartRouter.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const cartController  = require('../controllers/cartController');
+const cartController = require("../controllers/cartController");
 const { authenticate } = require("../middleware/authMiddleware");
 
 //For only user login
 router.use(authenticate);
 
-router.get('/',cartController.getCart);
-router.post('/add',cartController.addItem);
-router.delete('/item/:id',cartController.removeItem);
-
+router.get("/", cartController.getCart);
+router.post("/add", cartController.addItem);
+router.delete("/item/:id", cartController.removeItem);
+router.put("/item/:id", cartController.updateItemQuantity);
 module.exports = router;
