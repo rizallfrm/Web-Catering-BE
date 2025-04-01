6128816e099044ca83db764fb4cced983aa9b40b
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-04-01 23:30:14 +0700

    update: add cord to handle global hit

--- catering-be/app.js
+++ catering-be/app.js
@@ -1,8 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({
+  origin: " *", // Ganti dengan domain frontend kamu
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization"
+}));
+ //cors supaya bisa di hit api
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,19 +19,20 @@ app.get("/", (req, res) => {
   res.status(200).send({ message: "Ping Successfully" });
 });
 
-
-//Routes
+// Routes
 const userRouter = require("./routes/userRouter");
 const menuRouter = require("./routes/menuRouter");
 const cartRouter = require("./routes/cartRouter");
 const orderRouter = require("./routes/orderRouter");
 const adminRouter = require("./routes/adminRoutes");
+const uploadRouter = require("./routes/uploadRoutes");
 
-app.use('/api/menus', menuRouter);;
+app.use('/api/menus', menuRouter);
 app.use('/api/carts', cartRouter);
-app.use('/api/users/', userRouter);
+app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 app.use('/api/admin', adminRouter);
+app.use('/api/upload', uploadRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
