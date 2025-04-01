104253e847b3db20ef61e7a274561076ab40a6f5
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-04-01 23:34:02 +0700

    update: orderRouter to handle order

--- catering-be/routes/orderRouter.js
+++ catering-be/routes/orderRouter.js
@@ -1,13 +1,16 @@
-const express= require("express");
+const express = require("express");
 const router = express.Router();
-const orderController = require('../controllers/orderController')
-const {authenticate, authorize} = require('../middleware/authMiddleware')
+const orderController = require("../controllers/orderController");
+const { authenticate, authorize } = require("../middleware/authMiddleware");
 
 router.use(authenticate);
-router.post('/checkout', orderController.checkout)
-router.post('/my-orders',orderController.getMyOrders)
+// User routes
+router.post("/checkout", orderController.checkout);
+router.get("/my-orders", orderController.getMyOrders);
+router.get("/:id", orderController.getOrderById);
+router.put("/:id/cancel", orderController.cancelOrder);
 
 // Admin ubah status
-router.put('/:id/status', authorize(['admin']), orderController.updateStatus);
+router.put("/:id/status", authorize(["admin"]), orderController.updateStatus);
 
 module.exports = router;
