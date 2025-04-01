d0fbd9d512f2f284524b8932534c45dff38e5dc0
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-04-01 23:34:31 +0700

    update: models user

--- catering-be/models/user.js
+++ catering-be/models/user.js
@@ -1,40 +1,47 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-const { v4: uuidv4 } = require('uuid');
+"use strict";
+const { Model } = require("sequelize");
+const { v4: uuidv4 } = require("uuid");
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
-      User.hasMany(models.Cart, { foreignKey: 'user_id' });
-      User.hasMany(models.Order, { foreignKey: 'user_id' });
+      User.hasMany(models.Cart, { foreignKey: "user_id" });
+      User.hasMany(models.Order, { foreignKey: "user_id" });
     }
   }
 
-  User.init({
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true
+  User.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false, 
+        validate: {
+          notEmpty: true, 
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      password: DataTypes.STRING,
+      phone: DataTypes.STRING,
+      address: DataTypes.TEXT,
+      role: {
+        type: DataTypes.STRING,
+        defaultValue: "user",
+      },
     },
-    name: DataTypes.STRING,
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false
-    },
-    password: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    address: DataTypes.TEXT,
-    role: {
-      type: DataTypes.STRING,
-      defaultValue: 'user'
+    {
+      sequelize,
+      modelName: "User",
     }
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  );
 
   return User;
 };
