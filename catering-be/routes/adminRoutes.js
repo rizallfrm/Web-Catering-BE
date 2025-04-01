c5b61edea61dbce211816266edaf6a28c088e467
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-04-01 23:34:59 +0700

    update: adminRouter to handle admin

--- catering-be/routes/adminRoutes.js
+++ catering-be/routes/adminRoutes.js
@@ -2,13 +2,20 @@ const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/adminController");
 const { authenticate, authorize } = require("../middleware/authMiddleware");
+const orderController = require("../controllers/orderController");
 
 // Router semua untuk admin
+router.use(authenticate);
 router.use(authenticate, authorize(["admin"]));
 
+router.get("/dashboard", adminController.getDashboardStats);
+router.put("/users/:id", adminController.updateUser);
+router.put("/users/:id/role", adminController.changeUserRole);
+router.put("/users/:id/status", adminController.changeUserStatus);
 router.get("/users", adminController.getAllUsers);
 router.delete("/users/:id", adminController.deleteUser);
-router.get("/orders", adminController.getAllOrders);
 
+// Order Management
+router.get("/orders", adminController.getAllOrders);
 
 module.exports = router;
