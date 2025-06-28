19e1631c36cbec131e7c9f1476309e3d45674344
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-06-29 02:27:59 +0700

    update: modify orderRouter

--- catering-be/routes/orderRouter.js
+++ catering-be/routes/orderRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const orderController = require("../controllers/orderController");
 const { authenticate, authorize } = require("../middleware/authMiddleware");
+const upload = require("../config/multerConfig");
 
 router.use(authenticate);
 // User routes
@@ -9,6 +10,12 @@ router.post("/checkout", orderController.checkout);
 router.get("/my-orders", orderController.getMyOrders);
 router.get("/:id", orderController.getOrderById);
 router.put("/:id/cancel", orderController.cancelOrder);
+
+router.post(
+  "/:id/payment-proof",
+  upload.single("payment_proof"),
+  orderController.uploadPaymentProof
+);
 
 // Admin ubah status
 router.put("/:id/status", authorize(["admin"]), orderController.updateStatus);
