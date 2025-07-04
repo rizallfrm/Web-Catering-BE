b659476fff9d70f9006fe863e95bf59a91f0acd7
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-07-05 05:40:11 +0700

    update: order models migrations

--- catering-be/migrations/20250318200455-create-order.js
+++ catering-be/migrations/20250318200455-create-order.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Orders', {
+    await queryInterface.createTable("Orders", {
       id: {
         type: Sequelize.UUID,
         primaryKey: true,
@@ -10,24 +10,65 @@ module.exports = {
       user_id: {
         type: Sequelize.UUID,
         references: {
-          model: 'Users',
-          key: 'id'
+          model: "Users",
+          key: "id",
         },
-        onDelete: 'CASCADE'
+        onDelete: "CASCADE",
       },
       status: {
-        type: Sequelize.STRING,
-        defaultValue: 'pending'
+        type: Sequelize.ENUM(
+          "Menunggu Konfirmasi",
+          "Dikonfirmasi",
+          "Diproses",
+          "Dikirim",
+          "Selesai",
+          "Dibatalkan"
+        ),
+        defaultValue: "Menunggu Konfirmasi",
+      },
+      payment_status: {
+        type: Sequelize.ENUM(
+          "Belum Bayar",
+          "Menunggu Verifikasi",
+          "Sudah Dibayar",
+          "Dibatalkan"
+        ),
+        defaultValue: "Belum Bayar",
       },
       payment_proof: Sequelize.STRING,
-      total_price: Sequelize.INTEGER, 
+      total_price: Sequelize.INTEGER,
       proof_image_url: Sequelize.STRING,
-      wa_number: Sequelize.STRING, 
+
+      delivery_date: Sequelize.DATE,
+      delivery_address: Sequelize.STRING,
+      delivery_status: {
+        type: Sequelize.ENUM(
+          "Menunggu Jadwal",
+          "Sedang Diproses",
+          "Dalam Pengiriman",
+          "Terkirim",
+          "Dibatalkan"
+        ),
+        defaultValue: "Menunggu Jadwal",
+      },
+      delivery_notes: Sequelize.TEXT,
+      wa_number: Sequelize.STRING,
       createdAt: Sequelize.DATE,
-      updatedAt: Sequelize.DATE
+      updatedAt: Sequelize.DATE,
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Orders');
-  }
+    await queryInterface.dropTable("Orders");
+
+    // Hapus enum secara eksplisit agar tidak menyebabkan error saat migrate ulang
+    await queryInterface.sequelize.query(
+      'DROP TYPE IF EXISTS "enum_Orders_status";'
+    );
+    await queryInterface.sequelize.query(
+      'DROP TYPE IF EXISTS "enum_Orders_payment_status";'
+    );
+    await queryInterface.sequelize.query(
+      'DROP TYPE IF EXISTS "enum_Orders_delivery_status";'
+    );
+  },
 };
