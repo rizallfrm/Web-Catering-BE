94cc88a998c3026d0eb52d8b43202ce0173a473a
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-07-05 05:39:43 +0700

    update: modify order models

--- catering-be/models/order.js
+++ catering-be/models/order.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Order extends Model {
     /**
@@ -10,24 +8,61 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Order.belongsTo(models.User, { foreignKey: 'user_id' });
-      Order.hasMany(models.OrderItem, { foreignKey: 'order_id' });    }
+      Order.belongsTo(models.User, { foreignKey: "user_id" });
+      Order.hasMany(models.OrderItem, { foreignKey: "order_id" });
+    }
   }
-  Order.init({
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true
+  Order.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      user_id: DataTypes.UUID,
+      status: {
+        type: DataTypes.ENUM(
+          "Menunggu Konfirmasi",
+          "Dikonfirmasi",
+          "Diproses",
+          "Dikirim",
+          "Selesai",
+          "Dibatalkan"
+        ),
+        defaultValue: "Menunggu Konfirmasi",
+      },
+      payment_status: {
+        type: DataTypes.ENUM(
+          "Belum Bayar",
+          "Menunggu Verifikasi",
+          "Sudah Dibayar",
+          "Dibatalkan"
+        ),
+        defaultValue: "Belum Bayar",
+      },
+      total_price: DataTypes.INTEGER,
+      proof_image_url: DataTypes.STRING,
+      wa_number: DataTypes.STRING,
+
+      delivery_date: DataTypes.DATE,
+      delivery_address: DataTypes.STRING,
+      delivery_status: {
+        type: DataTypes.ENUM(
+          "Menunggu Jadwal",
+          "Sedang Diproses",
+          "Dalam Pengiriman",
+          "Terkirim",
+          "Dibatalkan"
+        ),
+        defaultValue: "Menunggu Jadwal",
+      },
+      delivery_notes: DataTypes.TEXT,
     },
-    user_id: DataTypes.UUID,
-    status: DataTypes.STRING,
-    total_price: DataTypes.INTEGER,
-    proof_image_url: DataTypes.STRING,
-    wa_number: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Order',
-  });
+    {
+      sequelize,
+      modelName: "Order",
+    }
+  );
   return Order;
 };
